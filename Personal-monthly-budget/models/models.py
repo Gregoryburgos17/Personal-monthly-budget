96d61dafdaca372b8858710fe65d7422f2a7926c
AlexSnowReDo <121876204+alexsnowredo@users.noreply.github.com>
2024-01-12 09:43:51 -0800

    [FIX] TIpos de Campos en la BD, Date y Float en vez de DateTime e Integer

--- Personal-monthly-budget/models/models.py
+++ Personal-monthly-budget/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Category(models.Model):
     _name = 'personal_monthly_budget.categorias'
@@ -12,7 +12,14 @@ class Finanza(models.Model):
     _description = 'Seguimiento Ingresos y Egresos'
 
     Descripcion = fields.Char(string='Descripcion', required=True)
-    Monto = fields.Integer(string='Monto', required=True)
-    Fecha = fields.Datetime()
+    Monto = fields.Float(string='Monto', required=True)
+    Fecha = fields.Date(required=True)
     Categoria = fields.Many2one('personal_monthly_budget.categorias', string = 'Categoria')
-    tipo = fields.Selection([('ingreso', 'Ingreso'), ('egreso', 'Egreso')], string='tipo', default='ingreso')
+    tipo = fields.Selection([('ingreso', 'Ingreso'), ('egreso', 'Egreso')], string='tipo', default='Ingreso')
+    display_monto = fields.Float(string='Monto', compute='_compute_display_monto', store=True, invisible=True)
+
+@api.depends('Monto', 'tipo')
+def _compute_display_monto(self):
+    for record in self:
+        # If tipo is 'egreso', make Monto negative; otherwise, keep it as is
+        record.display_monto = -record.Monto if record.tipo == 'egreso' else record.Monto
